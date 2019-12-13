e3fcfbeacbe8aada1ac4a7e42d471313222bc628
jjsjjsjjsjjs <jjs9241@naver.com>
2019-12-13 17:51:02 +0900

    수업종료

--- app.py
+++ app.py
@@ -3,11 +3,13 @@
 from flask import Flask, escape, request, render_template
 from decouple import config
 import requests
+import html
 
 app = Flask(__name__)
 
 api_url = f'https://api.telegram.org/bot'
 token = config('TELEGRAM_BOT_TOKEN')
+goole_key = config('GOOGLE_KEY')
 
 @app.route('/')
 def hello():
@@ -33,9 +35,43 @@ def send():
     print(res)
     return render_template('send.html')
 
-#@app.route('/telegram',method=['POST'])
-#def telegram():
-    #return 'ok'
+@app.route('/telegram',methods=['POST'])
+def telegram():
+    req = request.get_json()
+    print(req)
+    user_id = req['message']['from']['id'] 
+    user_input = req['message']['text']
+    method = '/sendMessage'
+    appended = f'?text={user_input}&chat_id={user_id}'
+    
+    if user_input == '로또':
+        return_data = '로또를 입력하셨습니다.'
+        appended = f'?text={return_data}&chat_id={user_id}'
+    elif user_input[0:3] == '번역 ':
+        google_api_url = 'https://translation.googleapis.com/language/translate/v2'
+        before_text = user_input[3:]
+
+        data = {
+            'q': before_text,
+            'source':'ko',
+            'target':'en',
+        }
+        request_url = f'{google_api_url}?key={goole_key}'
+
+        req = requests.post(request_url, data).json()
+        print(req)
+        return_data = html.unescape(req['data']['translations'][0]['translatedText'])
+        print(return_data)
+        appended = f'?text={return_data}&chat_id={user_id}'
+    else:
+        return_data = '무슨 말인지 모르겠어요. ㅠㅠ'
+        appended = f'?text={return_data}&chat_id={user_id}'
+
+    res = requests.get(api_url + token + method + appended).json()
+    print(res)
+    
+
+    return 'ok', 200 # 200을 텔레그램 서버에 보내지 않으면 텔레그램 서버는 요청을 계속 보내도록 되어 있다.
 
 if __name__ == '__main__':
     app.run(debug=True)
--- webhook.py
+++ webhook.py
@@ -2,7 +2,8 @@ from decouple import config
 
 #웹훅설정을 위한 경로
 token = config('TELEGRAM_BOT_TOKEN')
-ngrok_url = 'https://4cf338a5.ngrok.io'
+#ngrok_url = 'https://4cf338a5.ngrok.io/telegram'
+ngrok_url = 'https://jjs9341.pythonanywhere.com/telegram'
 
 #내가 연결하려는 주소
 url = f'https://api.telegram.org/bot{token}/setWebhook'
